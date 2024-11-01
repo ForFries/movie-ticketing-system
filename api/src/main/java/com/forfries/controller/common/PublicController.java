6331600a0127d3116d9e5af3689f3e939d24e410
fries <nolanzho@outlook.com>
2024-11-01 17:21:48 +0800

    修改了一些bug

--- api/src/main/java/com/forfries/controller/common/PublicController.java
+++ api/src/main/java/com/forfries/controller/common/PublicController.java
@@ -1,11 +1,16 @@
 package com.forfries.controller.common;
 
+import com.forfries.constant.NormalStatusConstant;
+import com.forfries.dto.MoviePageDTO;
+import com.forfries.result.PageResult;
+import com.forfries.result.Result;
 import com.forfries.service.CinemaService;
 import com.forfries.service.MovieService;
 import com.forfries.service.ScheduleService;
 import com.forfries.service.ScreeningHallService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +27,10 @@ public class PublicController {
     @Autowired
     private ScheduleService scheduleService;
 
-
+    @GetMapping("/movies")
+    Result<PageResult> pageMovies(MoviePageDTO moviePageDTO) {
+        moviePageDTO.setStatus(NormalStatusConstant.NORMAL_STATUS);
+        return Result.success(movieService.page(moviePageDTO));
+    }
 
 }
